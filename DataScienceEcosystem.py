c055588ffd8fb9fa70f6e2e79fc551686d3b1b81
naamsin <136036172+naamsin@users.noreply.github.com>
2023-06-10 13:00:54 -0700

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ### Some of the popular languages that Data Scientists use are:
+# 
+# 1. R
+# 2. Python
+# 3. SQL
+# 
+
+# ### Some of the commonly used libraries used by Data Scientists include:
+# 1. Tensorflow
+# 2. NumPy
+# 3. Keras
+
+# ### 
+# | Data Science Tools |
+# | ------- |
+# | Juypter Notebook |
+# | R Studio |
+# | SQlite |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+# 
+# 1 + 3
+# 4/2
+# 3 * 6
+
+# In[5]:
+
+
+## This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[8]:
+
+
+## This will convert 200 minutes to hours by diving by 60
+200/60 
+
+
+#  __Objectives:__
+#  - forumlate arithemetic expressions
+#  - learn about the data science tools
+#  - create a juypter notebook
+
+# ## Author
+# ### Naamveer Singh
+# 
